877706a3dc4f63b1b716aa69ddfdc2bdac75e9c6
Mariam Alabkari <55929646+mariamalabkari@users.noreply.github.com>
2021-12-03 21:03:48 +0300

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,160 +1,118 @@
-
-
-
 # import libraries
-import nltk
-from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
+import sys
 import pickle
+import nltk
 import re
-from sklearn.svm import SVC
+import warnings
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.metrics import confusion_matrix, classification_report, hamming_loss
-from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
-nltk.download(['punkt', 'wordnet', 'stopwords', 'averaged_perceptron_tagger'])
+from sklearn.externals import joblib
+from sklearn.metrics import classification_report, accuracy_score,  precision_score, recall_score, f1_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import LinearSVC
+nltk.download(['punkt', 'wordnet'])
+warnings.filterwarnings("ignore")
 
 
-# load data from database
-engine = create_engine('sqlite:////content/sample_data/Database.db')
-df = pd.read_sql("SELECT * FROM DatabaseTable", engine)
-df.related.replace(2,1,inplace=True)
-X = df['message']
-Y = df.drop(['id', 'message', 'original', 'genre'], axis = 1)
-#/content/sample_data/Database.db
-
+def load_data(database_filepath):
+    '''
+    input:
+        database_filepath = File path of SQL database
+    output:
+    X = Features dataframe
+    Y =  Target dataframe
+    category_names list = Target labels 
+    '''
+    # load data from database
+    engine = create_engine('sqlite:///'+ database_filepath)
+    df = pd.read_sql_table('CleanTable', engine)
+    X = df.message.values
+    Y = df[df.columns[4:]].values
+    category_names = list(df.columns[4:])
+    return X, Y, category_names
 
 
 def tokenize(text):
-    
-    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    detected_urls = re.findall(url_regex, text)
-    for url in detected_urls:
-        text = text.replace(url, "urlplaceholder")
-    
+    '''
+    input:
+        text = Messages for tokenization.
+    output:
+        clean_tokeni = list after tokenization.
+    '''
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
     clean_tokens = []
     for tok in tokens:
-       
-        # Remove stop words
-        if tok in stopwords.words("english"):
-            continue
-        # Reduce words to their stems
-        tok = PorterStemmer().stem(tok)
-        # Reduce words to their root form
-        tok = lemmatizer.lemmatize(tok).lower().strip()
-
-        clean_tokens.append(tok)
-        
-    clean_tokens = [tok for tok in clean_tokens if tok.isalpha()]
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
     return clean_tokens
 
-print(X[3])
-print(tokenize(X[3]))
-
-
-
-# Build a machine learning pipeline
-pipeline = Pipeline([
-        ('vect', CountVectorizer(tokenizer=tokenize)),
-        ('tfidf', TfidfTransformer()),
-        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+def build_model():
+    pipeline = Pipeline([('cvect', CountVectorizer(tokenizer = tokenize)),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', MultiOutputClassifier(OneVsRestClassifier(LinearSVC())))
     ])
+    parameters = {'clf__estimator__estimator__C': [1, 2, 5]}
+    cv =  GridSearchCV(pipeline, param_grid=parameters, verbose=3, cv = 5)
+    return cv
 
 
+def evaluate_model(model, X_test, y_test, category_names):
+    # Predict categories of messages.
+    y_pred = model.predict(X_test)
+    # Print accuracy score, precision score, recall score and f1_score for each categories
+    for i in range(0, len(category_names)):
+        print(category_names[i])
+        print("\tAccuracy: {:.4f}\t|| Precision: {:.4f}\t|| Recall: {:.4f}\t|| F1_score:                            {:.4f}".format(
+            accuracy_score(y_test[:, i], y_pred[:, i]),
+            precision_score(y_test[:, i], y_pred[:, i], average='weighted'),
+            recall_score(y_test[:, i], y_pred[:, i], average='weighted'),
+            f1_score(y_test[:, i], y_pred[:, i], average='weighted')
+        ))
 
-# Train pipeline
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
- # train classifier
-pipeline.fit(X_train, Y_train)
-
-
-
-# Test your model
-Y_pred = pipeline.predict(X_test)
-
-for ix, col in enumerate(Y.columns):
-    print(col)
-    print(classification_report(Y_test[col], Y_pred[:,ix]))
-
-avg = (Y_pred == Y_test).mean().mean()
-print("Accuracy Overall:\n", avg)
-
+def save_model(model, model_filepath):
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
-# Improve model
-pipeline.get_params
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
 
-parameters = {
-        'vect__max_df':[1, 5],
-        'clf__estimator__n_estimators': [20, 50]
-    }
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
 
+        print('Trained model saved!')
 
-cv = GridSearchCV(pipeline, param_grid=parameters, verbose=10)
-cv.fit(X_train, Y_train)
-
-
-
-# test model
-
-Y_pred = cv.predict(X_test)
-for ix, col in enumerate(Y.columns):
-    print(col)
-    print(classification_report(Y_test[col], Y_pred[:,ix]))
-
-avg = (Y_pred == Y_test).mean().mean()
-print("Accuracy Overall:\n", avg)
-
-
-
-# improve model
-# using SVM instead 
-pipe2 = Pipeline([
-    ('vect', CountVectorizer(tokenizer = tokenize)),
-    ('tfidf', TfidfTransformer()),
-    ('clf', MultiOutputClassifier(SVC()))
-])
-
-parameters2 = {'vect__min_df': [5],
-              'tfidf__use_idf':[True],
-              'clf__estimator__kernel': ['poly'], 
-              'clf__estimator__degree': [1, 2, 3],
-              'clf__estimator__C':[1, 10, 100]}
-
-cv2 = GridSearchCV(pipe2, param_grid = parameters2, verbose = 10)
-
-# Find best parameters
-np.random.seed(77)
-T_model2 = cv2.fit(X_train, Y_train)
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
-
-# grid search results
-T_model2.cv_results_
-
-
-
-# Calculate evaluation metrics for test set
-tuned_pred_test2 = T_model2.predict(X_test)
-
-eval_metrics2 = eval_metrics(np.array(Y_test), tuned_pred_test2, col_names)
-
-print(eval_metrics2)
-
-
-
-# Export model as a pickle file
-pickle.dump(cv, open("classifier.pkl", 'wb'))
-
+if __name__ == '__main__':
+    main()
